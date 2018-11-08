a511a3aa9f51c29686847a028a4259bb40e16afc
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-11-08 22:43:45 +0000

    Added room shell and objects, began implementing player character and interaction

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/*
+ * The code in this script is based on a publically available YouTube tutorial produced by user Brackeys (2017)
+ * For the full reference to this source, along with others used for this Design Challenge 01, see Documentation\Design Challenge 01\3rd Party Assets\Design Challenge 01 Reference List.pdf
+ * */
+
+public class AudioManager : MonoBehaviour
+{
+    [System.Serializable]
+    class Sound
+    {
+        public string name = null;
+
+        public AudioClip clip = null;
+
+        [Range(0.0f, 1.0f)]
+        public float volume = 1.0f;
+        [Range(0.1f, 3f)]
+        public float pitch = 1.0f;
+
+        public bool loop = false;
+        public bool playOnAwake = false;
+
+        [HideInInspector]
+        public AudioSource source = null;
+    }
+
+    public static AudioManager instance;
+
+    private List<string> m_currentlyPlayingSounds = new List<string>();
+
+    [SerializeField]
+    private Sound[] m_sounds;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        AddSounds();
+    }
+
+    private void AddSounds()
+    {
+        for (int i = 0; i < m_sounds.Length; i++)
+        {
+            m_sounds[i].source = gameObject.AddComponent<AudioSource>();
+            m_sounds[i].source.clip = m_sounds[i].clip;
+            m_sounds[i].source.volume = m_sounds[i].volume;
+            m_sounds[i].source.pitch = m_sounds[i].pitch;
+            m_sounds[i].source.loop = m_sounds[i].loop;
+            m_sounds[i].source.playOnAwake = m_sounds[i].playOnAwake;
+        }
+    }
+
+    public void Play(string _name)
+    {
+        for (int i = 0; i < m_sounds.Length; i++)
+        {
+            if (m_sounds[i].name == _name)
+            {
+                if (m_sounds[i].source != null && !m_sounds[i].source.isPlaying)
+                {
+                    m_sounds[i].source.Play();
+                    Debug.Log("Playing audio: " + _name);
+                    m_currentlyPlayingSounds.Add(_name);
+                    Debug.Log("Audio added: " + _name);
+                }
+                else if (m_sounds[i].source.isPlaying)
+                {
+                    Debug.Log("Sound: " + _name + " is already playing");
+                }
+                else
+                {
+                    Debug.Log("Sound: " + _name + " was not found.");
+                }
+            }
+        }
+    }
+
+    public void Stop(string _name)
+    {
+        for (int i = 0; i < m_sounds.Length; i++)
+        {
+            if (m_sounds[i].name == _name)
+            {
+                if (m_sounds[i].source.isPlaying)
+                {
+                    m_sounds[i].source.Stop();
+                    Debug.Log("Audio Stopped: " + _name);
+                    if (m_currentlyPlayingSounds.Contains(_name))
+                    {
+                        m_currentlyPlayingSounds.Remove(_name);
+                        Debug.Log("Audio Removed: " + _name);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Sound: " + _name + " is not playing");
+                }
+            }
+        }
+    }
+
+    public void Pause(string _name)
+    {
+        for (int i = 0; i < m_sounds.Length; i++)
+        {
+            if (m_sounds[i].name == _name)
+            {
+                if (m_sounds[i].source.isPlaying)
+                {
+                    m_sounds[i].source.Pause();
+                    Debug.Log("Sound: " + _name + " paused");
+                }
+            }
+        }
+    }
+
+    public void StopAll()
+    {
+        for (int i = 0; i < m_sounds.Length; i++)
+        {
+            if (m_sounds[i].source.isPlaying)
+            {
+                m_sounds[i].source.Stop();
+                m_currentlyPlayingSounds.Clear();
+            }
+        }
+
+        Debug.Log("All audio stopped");
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField]
+    float m_sphereRad = 10.0f;
+    [SerializeField]
+    float m_maxInteractDist = 50.0f;
+    [SerializeField]
+    LayerMask m_interactLayers;
+
+    RaycastHit m_hit;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (Physics.SphereCast(gameObject.transform.position, m_sphereRad, transform.forward, out m_hit, m_interactLayers))
+            {
+                Debug.Log("Hit");
+            }
+
+            // Check contents of hit, use interface to interact
+        }
+    }
+}
